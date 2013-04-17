85eb439126bd5086d531b7208f95b39710d41b30
Randall Degges <rdegges@gmail.com>
2013-04-17 02:01:23 -0700

    Adding btc module.

---
+++ btc.py
@@ -0,0 +1,9 @@
+"""
+    btc
+    ~~~
+
+    Buy, sell, and transfer bitcoin instantly at your terminal! (Powered by
+    Coinbase: https://coinbase.com/).
+
+    For help, run `btc` with no parameters.
+"""
