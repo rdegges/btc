53a3889104ad906710861ced79311e001035fc32
Randall Degges <rdegges@gmail.com>
2013-04-17 19:01:51 -0700

    First attempt at a proper setup script.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'btc',
+    version = '0.1',
+    scripts = ('btc', ),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['docopt>=0.6.1', 'requests>=1.2.0'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'rdegges@gmail.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/btc',
+    keywords = 'bitcoin cli coinbase forex utility',
+    description = 'Buy, sell, and transfer bitcoin instantly in your terminal!',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.md'))).read()
+
+)
